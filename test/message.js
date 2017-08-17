b47ba0bb0c0bc5f4f84f56e334d417180d7c7627
romanornr <romanornr@gmail.com>
2017-08-17 12:57:12 +0200

    Viacoin: test verify with uncompressed pubkey

--- test/message.js
+++ test/message.js
@@ -101,7 +101,7 @@ describe('Message', function() {
   });
 
   it('will verify with an uncompressed pubkey', function() {
-    var privateKey = new bitcore.PrivateKey('5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss');
+    var privateKey = new bitcore.PrivateKey('7h2CEmJtwtqQBqrEKkzTXd57zFUyc9CDiyQamCenhiaYxX5yUU8');
     var message = new Message('This is an example of a signed message.');
     var signature = message.sign(privateKey);
     var verified = message.verify(privateKey.toAddress(), signature);
